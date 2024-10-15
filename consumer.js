61341e1ad74ecd949f765916f1ffd07dcf668cda
awakko <fulazmi@gmail.com>
2024-10-16 03:23:19 +0700

    add snappy

--- consumer.js
+++ consumer.js
@@ -1,10 +1,17 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 import fs from 'fs';
 import { Kafka } from 'kafkajs';
+const { CompressionTypes, CompressionCodecs } = require('kafkajs')
+const SnappyCodec = require('kafkajs-snappy')
 
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const topicName = process.env['KAFKA_TOPIC'];
+CompressionCodecs[CompressionTypes.Snappy] = SnappyCodec
 const kafka = new Kafka({
     clientId: 'nodeJS-app',
     brokers: [process.env['KAFKA_BROKER']],
@@ -14,15 +21,28 @@ const kafka = new Kafka({
       username: process.env['KAFKA_SASL_USERNAME'],
       password: process.env['KAFKA_SASL_PASSWORD']
     },
+    connectionTimeout: 60000,
   })
+kafka.CompressionCodecs=CompressionCodecs;
 
 /* ================== Consumer Configuration ============= */
 const consumer = kafka.consumer({
   groupId: process.env['KAFKA_GROUP'],
-  sessionTimeout: 30000, //default
-  rebalanceTimeout: 60000, //default
-  heartbeatInterval: 3000, //default
-  maxWaitTimeInMs: 50000,  //default
+  sessionTimeout: 60000,
+  heartbeatInterval: 10000,
+ 
+  // NEGATIVE_CASE INFINITE REBALANCE
+  // sessionTimeout: 20000,
+  // heartbeatInterval: 19000,
+
+
+  retry: {
+    retries: 5,  // jumlah retry sebelum dianggap gagal
+    initialRetryTime: 300,  // waktu retry pertama dalam milidetik
+    factor: 0.2,  // faktor kenaikan waktu retry
+    multiplier: 2,  // pengali waktu retry
+  },
+ 
 
   // metadataMaxAge: <Number>,
   // allowAutoTopicCreation: <Boolean>,
@@ -36,7 +56,7 @@ const consumer = kafka.consumer({
   //more info = https://kafka.js.org/docs/consuming#options
 })
 await consumer.connect()
-await consumer.subscribe({ topic: topicName, fromBeginning: true })
+await consumer.subscribe({ topic: topicName, fromBeginning: false })
 await consumer.run({
     eachMessage: async ({ topic, partition, message}) => {
       console.log(`Received message:`, {
@@ -46,5 +66,6 @@ await consumer.run({
         partition: partition,
         offset: message.offset,
       })
+      // await sleep(3000);
     },
 })
